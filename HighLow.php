d964cfd15a3d45f03911234492776ba2cc16f2da
Mars <marianomarquis@gmail.com>
2015-07-14 08:12:48 -0500

    Fresh

--- HighLow.php
+++ HighLow.php
@@ -1,24 +1,28 @@
 <?php
 function HighLow () {
-    fwrite(STDOUT, "wanna play?" . PHP_EOL);
     $gamePicks = rand( 1, 100);
-    fwrite(STDOUT, "Guess a Digit, One to a Hundred" . PHP_EOL);
+    fwrite(STDOUT, "Guess a Digit, 1 ~ 100" . PHP_EOL);
     $usersGuess = fgets(STDIN);
+    $usersGuessCount = 0;
       do {
         if ($usersGuess > $gamePicks) {
           fwrite(STDOUT, "2High" . PHP_EOL);
           $usersGuess = fgets(STDIN);
+          $usersGuessCount +=1;
         } elseif ($usersGuess < $gamePicks) {
            fwrite(STDOUT, "2Low" . PHP_EOL);
            $usersGuess  = fgets(STDIN);
+           $usersGuessCount +=1;
          }
       } while ($usersGuess != $gamePicks);
 
       if ($usersGuess == $gamePicks) {
-           fwrite(STDOUT, 'Whoa Nice one' . PHP_EOL);
-           fwrite(STDOUT, )
-           GameOn();
-      }  
+          $usersGuessCount +=1;
+          echo ("It took {$usersGuessCount} attempts" . PHP_EOL);
+          $usersGuessCount = 0;
+          fwrite(STDOUT, 'Whoa Nice one' . PHP_EOL);
+          GameOn();
+      }
 }
 function GameOn () {
   fwrite(STDOUT, 'Play Again? y/n');
